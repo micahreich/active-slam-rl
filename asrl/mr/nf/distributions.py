bcdf8f1a835d9356c9086ae9c91c4e0ddf5ddd8e
Micah Reich <micahreich02@gmail.com>
2025-03-20 00:15:40 +0000

    working on toy code (rid spurious prints)

---
+++ asrl/mr/nf/distributions.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from normflows.distributions.base import ConditionalDiagGaussian
+
+
+class ContextEncoderDiagGaussian(nn.Module):
+    def __init__(self, shape, log_scale: nn.Module, loc: nn.Module):
+        super().__init__()
+        
+        if isinstance(shape, int):
+            self.shape = (shape,)
+        if isinstance(shape, list):
+            self.shape = tuple(shape)
+            
+        self.log_scale = log_scale
+        self.loc = loc
+    
+    def forward(self, context):
+        B = context.shape[0]
+        
+        mean = self.loc(context) if self.loc is not None else torch.zeros((B,) + self.shape, device=context.device)
+        log_scale = self.log_scale(context) if self.log_scale is not None else torch.zeros((B,) + self.shape, device=context.device)
+        
+        return torch.cat([mean, log_scale], dim=-1)
+
+
+class ConditionalDiagLinearGaussian(ConditionalDiagGaussian):
+    """
+    Conditional multivariate Gaussian distribution with diagonal
+    covariance matrix, parameters are obtained by a context encoder,
+    context meaning the variable to condition on
+    """
+    def __init__(self, shape, context_encoder: nn.Module, sigma_min=-5, sigma_max=-0.3):
+        """Constructor
+
+        Args:
+          shape: Tuple with shape of data, if int shape has one dimension
+          context_encoder: Computes mean and log of the standard deviation
+          of the Gaussian, mean is the first half of the last dimension
+          of the encoder output, log of the standard deviation the second
+          half
+        """
+        super().__init__(shape, context_encoder)
+
+        self.const = torch.tensor(-0.5 * np.prod(shape) * np.log(2 * np.pi))
+        self.sigma_min = sigma_min
+        self.sigma_max = sigma_max
+
+    def get_mean_std(self, context):
+        encoder_output = self.context_encoder(context)
+        split_ind = encoder_output.shape[-1] // 2
+        mean = encoder_output[..., :split_ind]
+        log_scale = encoder_output[..., split_ind:]
+        log_scale = torch.tanh(log_scale)
+        log_scale = self.sigma_min + 0.5 * (self.sigma_max - self.sigma_min) * (log_scale + 1)
+        sigma = log_scale.exp()
+        return mean, sigma
+
+    def forward(self, num_samples=1, context=None):
+        eps = torch.randn((num_samples,) + self.shape, dtype=context.dtype, device=context.device)
+        mean, std = self.get_mean_std(context)
+        z = mean + std * eps
+        log_p = self.const - torch.sum(
+            torch.log(std) + 0.5 * torch.pow(eps, 2), list(range(1, self.n_dim + 1))
+        )
+        return z, log_p
+    
+    def log_prob(self, z, context):
+        mean, std = self.get_mean_std(context)
+        log_p = self.const - torch.sum(
+            torch.log(std) + 0.5 * torch.pow((z - mean) / std, 2),
+            list(range(1, self.n_dim + 1)),
+        )
+        return log_p
+    
+    def get_qv(self, z, context):
+        mean, std = self.get_mean_std(context)
+        q = -torch.sum(0.5 * torch.pow((z - mean) / std, 2),
+                       list(range(1, self.n_dim + 1)))
+        v = self.const - torch.sum(torch.log(std), list(range(1, self.n_dim + 1)))
+        return q, -v
---
+++ asrl/mr/nf/transforms.py
@@ -0,0 +1,106 @@
+import torch
+import numpy as np
+from normflows.flows import Flow
+from torch.distributions import Transform, TanhTransform, AffineTransform, ComposeTransform, transforms
+
+
+class arcTanh(Flow):
+    def __init__(self):
+        super().__init__()
+        self.eps = np.finfo(np.float32).eps.item()
+
+    def forward(self, z):
+        z_ = torch.tanh(z)
+        log_det = torch.log(1-z_.pow(2) + self.eps).sum(-1, keepdim=False)
+        return z_, log_det
+
+    @torch.jit.export
+    def inverse(self, z):
+        z_ = torch.atanh(z)
+        log_det = -torch.log(1-z.pow(2) + self.eps).sum(-1, keepdim=False)
+        return z_, log_det
+
+class Clip(Flow):
+    def __init__(self, eps=1e-5):
+        super().__init__()
+        self.eps = eps
+
+    def forward(self, z_):
+        # (Generation direaction) output must be [-1, 1] (this is defined according to the env.)
+        z_ = torch.clamp(z_, -1, 1) 
+        return z_, torch.zeros(z_.shape[0], device=z_.device)
+    
+    @torch.jit.export
+    def inverse(self, z):
+        # (Density estimation direction) input must be [-1+esp, 1-esp] (prevent NAN outputs after preprocessing operation.)
+        z = torch.clamp(z, -1+self.eps, 1-self.eps) 
+        return z, torch.zeros(z.shape[0], device=z.device)
+    
+class Clip(Transform):
+    domain = transforms.constraints.real
+    codomain = transforms.constraints.real
+    bijective = True
+    sign = 1
+    
+    def __init__(self, lo, hi):
+        super().__init__()
+        self.lo = lo
+        self.hi = hi
+
+    def _call(self, x):
+        """ Forward transformation: y = s * tanh(x) + m """
+        return x
+
+    def _inverse(self, y):
+        """ Inverse transformation: x = atanh((y - m) / s) """
+        return torch.clip(y, self.lo, self.hi)
+
+    def log_abs_det_jacobian(self, x, y):
+        """ Log-determinant of the Jacobian """
+        return torch.tensor(0)
+    
+class Preprocessing(Flow):
+    def __init__(self):
+        super().__init__()
+        # trans = [arcTanh(), Clip(eps=1e-5)]
+        trans = [TanhTransform()]
+        # self.trans = torch.nn.ModuleList(trans)
+        # self.tanh_transform = TanhTransform()
+        eps = 1e-6
+        self.transform = ComposeTransform(parts=[TanhTransform(), Clip(-1+eps, 1-eps)])
+
+    def forward(self, z, context=None):
+        # clip_lo = self.tanh_transform.codomain.lower_bound + 1e-5
+        # clip_hi = self.tanh_transform.codomain.upper_bound - 1e-5
+        
+        _z = self.transform(z)
+        log_det = self.transform.log_abs_det_jacobian(z, _z).sum(-1, keepdim=False)
+        
+        return _z, log_det
+        # log_det = torch.zeros(z.shape[0], device=z.device)
+        # for flow in self.trans:
+        #     z = flow(z)
+        #     z, log_d = flow.forward(z)
+        #     log_det += log_d
+        # return z, log_det
+
+    @torch.jit.export
+    def inverse(self, z, context=None):
+        # clip_lo = self.tanh_transform.codomain.lower_bound + 1e-5
+        # clip_hi = self.tanh_transform.codomain.upper_bound - 1e-5
+        
+        _z = self.transform.inv(z)
+        log_det = self.transform.inv.log_abs_det_jacobian(z, _z).sum(-1, keepdim=False)
+        
+        return _z, log_det
+        # log_det = torch.zeros(z.shape[0], device=z.device)
+        # for flow in self.trans[::-1]:
+        #     z, log_d = flow.inverse(z)
+        #     log_det += log_d
+        # return z, log_det
+    
+    @torch.jit.export
+    def get_qv(self, z, context):
+        z_, q = self.inverse(z, context)
+        v = torch.zeros(z.shape[0], device=z.device)
+        return z_, q, v
