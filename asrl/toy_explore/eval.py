c7570e4af6d83b0b032950cd3658a4b47282d4bd
Micah Reich <micahreich02@gmail.com>
2025-04-17 03:39:49 +0000

    trained model with eval visualizer

---
+++ asrl/toy_explore/eval.py
@@ -0,0 +1,32 @@
+import os
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import yaml
+import gymnasium as gym
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+from asrl.toy_explore.env import GridExploreEnv
+from stable_baselines3 import PPO
+
+
+if __name__ == "__main__":
+    # Load from file
+    model = PPO.load("/home/dev/workspace/asrl/toy_explore/ppo_grid_explore.zip")
+
+    env = GridExploreEnv()
+    obs, _ = env.reset()
+
+    done, truncated = False, False
+
+    while not (done or truncated):
+        action, _states = model.predict(obs, deterministic=False)
+        obs, reward, done, truncated, info = env.step(action)
+        env.render()
+        time.sleep(1 / 30.0)
+
+    env.close()    
